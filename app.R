02309748a0aaef15b61bc24f47e80232408f896b
Zagreb Mukerjee <zagrebmukerjee@gmail.com>
2021-06-11 17:31:04 -0400

    app.R changes

--- app.R
+++ app.R
@@ -1,97 +1,157 @@
 
 
 library(shiny)
+library(shinyBS)
+library(shinyjs)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(DT)
 
-bernDraws <- function(piParam, nTrials){
-    
-    random <- runif(nTrials) # n i.i.d. uniform draws
-    outcome <- ifelse(random <= piParam, 1, 0) # how many < pi
-    
-    # cat(outcome, "\n")
-    
-    return(outcome)
-}
-
-bernMLE <- function(outcome, intervals = 20){
-    
-    nVal <- length(outcome) # Turn number of successes into K
-    nSuccesses <- sum(outcome)
-    
-    testPiParam <- (1:intervals)/intervals
-    probOutcomeGivenPi <- log((testPiParam^(nSuccesses))*((1-testPiParam)^(nVal - nSuccesses)))
-    
-    return <- data.frame(Pi = testPiParam, Likelihood = probOutcomeGivenPi)
-    
-}
-
-
+source("BernoulliHelpers.R")
+source("generalHelpers.R")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+    shinyjs::useShinyjs(),
     withMathJax(),
-
+    
     # Application title
-    titlePanel("Bernoulli Distribution"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("piParam",
-                        "Pi (Probability of Success):",
-                        min = 0,
-                        max = 1,
-                        value = .5,
-                        step = .1),
-            sliderInput("nTrials",
-                        "Number of Observations:",
-                        min = 1,
-                        max = 100,
-                        value = 50,
-                        step = 1),
-            uiOutput("distr"),
-            uiOutput("likelihood"),
-            uiOutput("logLikelihood")
+    titlePanel("Distribution Explorer"),
+    
+    selectInput(
+        "distrID",
+        "Select Distribution",
+        c("Bernoulli")
+    ),
+    
+    fluidRow(
+        h3("Known Distribution", style = "padding:15px"),
+        column(4,
+               sliderInput("piParamDistr",
+                           "Known Pi (Probability of Success):",
+                           min = 0,
+                           max = 1,
+                           value = .5,
+                           step = .1
+               ),
+               uiOutput("distr")
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot"),
-           plotOutput("MLEPlot")
+        
+        column(6, 
+               plotOutput("distPlot", height = "400px")
         )
+    ),
+    hr(),
+    
+    fluidRow(
+        h3("Data Generation Process", style = "padding:15px"),
+        column(4, sliderInput("piParam",
+                              "Pi (Probability of Success):",
+                              min = 0,
+                              max = 1,
+                              value = .5,
+                              step = .1),
+               sliderInput("nTrials",
+                           "Number of Observations:",
+                           min = 1,
+                           max = 500,
+                           value = 250,
+                           step = 1),
+               div(style="display:inline-block",
+                   actionButton(inputId = "generateDataButton",
+                                label = "Generate Data",
+                                icon("play-circle")
+                   ),
+                   bsTooltip(
+                       "generateDataButton", 
+                       "After defining distribution parameters, press here to generate data and display a sample",
+                       placement = "bottom", 
+                       trigger = "hover"
+                   )
+                   
+               )
+        ),
+        column(6,
+               h4("Data Snapshot"),
+               textOutput("outcomeDisplay"))
+    ),
+    hr(),
+    fluidRow(
+        h3("Likelihood", style = "padding:15px"),
+        column(4,
+               uiOutput("likelihood"),
+               uiOutput("logLikelihood")
+        ),
+        column(6,
+               plotOutput("MLEPlot", height = "400px")
+               )
     )
+  
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-
+server <- function(input, output, session) {
+    
     output$distPlot <- renderPlot({
         
         analyticalDistr <- data.frame(
             drawVal = factor(c("Successes", "Failures"), levels = c("Successes", "Failures")),
-            prob = c(input$piParam, 1-input$piParam)
+            prob = c(input$piParamDistr, 1-input$piParamDistr)
         )
         
         ggplot(analyticalDistr, aes(x = drawVal, y = prob, fill = drawVal)) + geom_bar(stat="identity") +
             scale_fill_manual(values=c("#56B4E9", "#E69F00")) +
-            labs(title = "Distribution", x= "y", y = "P(y)")+
+            labs(title = "Distribution (Analytically)", x= "y", y = "P(y)")+
             theme_minimal() +
-            theme(legend.position = "none")
+            theme(legend.position = "none", aspect.ratio=.8)
         
     })
+
     
+    observeEvent(
+        eventExpr = {
+            input$generateDataButton
+        },
+        handlerExpr = {
+            
+            outcomeData <<- bernDraws(piParam = input$piParam, nTrials = input$nTrials)
+            
+            # output$outcomeData <- outcomeData
+            
+            withCallingHandlers({
+                shinyjs::html("outcomeDisplay", "")
+                bernDataPrintHelper(outcomeData, 144)
+            },
+            message = function(m) {
+                shinyjs::html(id = "outcomeDisplay", html = m$message, add = TRUE)
+            })
+        }
+        
+    )
     
-    output$MLEPlot <- renderPlot({
-        outcome <- bernDraws(piParam = input$piParam, nTrials = input$nTrials) 
+    observeEvent(
+        eventExpr = {
+            input$generateDataButton
+        },
+        handlerExpr = {
+            
+            outcomeData <<- bernDraws(piParam = input$piParam, nTrials = input$nTrials)
+            
+            # output$outcomeData <- outcomeData
+            
+            output$MLEPlot <- renderPlot({
+                outcome <- outcomeData
+                
+                likelihoodDB <- bernMLE(outcome = outcome, intervals = 100) %>% rename(`Log Likelihood` = Likelihood)
+                
+                ggplot(likelihoodDB, aes(x = Pi, y = `Log Likelihood`)) + geom_line(color = "steelblue") +
+                    theme_minimal()
+                
+            })
+        }
         
-        likelihoodDB <- bernMLE(outcome = outcome, intervals = 100) %>% rename(`Log Likelihood` = Likelihood)
-         
-        ggplot(likelihoodDB, aes(x = Pi, y = `Log Likelihood`)) + geom_line(color = "steelblue") +
-            theme_minimal()
-        
-    })
+    )
     
     output$distr <- renderUI({
         withMathJax(helpText("Bernoulli PMF: $$P(y|\\pi) = \\pi^y(1-\\pi)^{{(1-y)}}$$"))

e7f2eddaaa08d957e691f5de5984336d2f744c62
Zagreb Mukerjee <zagrebmukerjee@gmail.com>
2021-07-21 22:04:17 -0400

    Latex typo fix

--- DistributionSpecific/LogNormHelpers.R
+++ DistributionSpecific/LogNormHelpers.R
@@ -49,8 +49,8 @@ logNormLatex <- function(type){
   }
   else if(type == "Model"){
     
-    withMathJax("Statistical Model: Log-Normal \\begin{aligned}
-Y_i &\\sim \\text{Log-Normal}}(y_i |\\mu_i) \\\\
+    withMathJax("Statistical Model: Log Normal \\begin{aligned}
+Y_i &\\sim \\text{LogNormal} (y_i |\\mu_i) \\\\
 \\mu_i &= \\beta  \\\\  
 Y_i &\\perp \\!\\!\\! \\perp Y_j \\quad \\forall \\: i \\neq j \\\\
 \\end{aligned}")

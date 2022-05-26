f1094e7bb88171e96f208fd38faa9e9118b47500
zagrebmukerjee <zagrebmukerjee@gmail.com>
2022-05-26 19:43:03 -0400

    aaa

--- R/appFunction.R
+++ R/appFunction.R
@@ -3,12 +3,11 @@
 #' @export
 run2k1 <- function()  {
   
-  tutorialText <- read.csv("data/TutorialInfo.csv")
-  
   #HACK
   # source("R/preamble.R")
   a<- shiny::shinyApp(ui = ui2k1, server = server,
            onStart = function(){
+
              oldw <<- getOption("warn")
              options(warn = -1)#, shiny.fullstacktrace = T)
              onStop(function(){
